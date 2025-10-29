4414bc6e0487a72992d5b945e8a0848c5fc8b1b4
davidgd72k <davidgd72k@gmail.com>
2025-10-29 23:11:39 +0100

    Finished

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,11 +1,14 @@
 extends Node3D
 
 @onready var orbital_pivot: Node3D = $OrbitalPivot
+@onready var camera_3d: Camera3D = $Camera3D
+@onready var color_rect: ColorRect = $CanvasLayer/ColorRect
 
 func _ready() -> void:
-	pass
+	color_rect.show()
 
 
 func _physics_process(delta: float) -> void:
-	orbital_pivot.rotate_y(1 * delta)
+	orbital_pivot.rotate_y(1.0 * delta)
+	
 	pass
